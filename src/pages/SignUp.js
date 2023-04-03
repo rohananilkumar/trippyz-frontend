c04d592c2e36aa5ca283fc908651435f7f5ac350
ROHAN <ro.anilpnr@gmail.com>
2023-04-03 19:33:42 +0530

    initial commit

---
+++ src/pages/SignUp.js
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./SignUp.module.scss";
+
+const SignUp = () => {
+  return <div className={styles.signUp}>Hello world</div>;
+};
+export default SignUp;
