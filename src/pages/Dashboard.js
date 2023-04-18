e02a5190b5a4b385214e997b14311c16802ff136
AthullyaR <78737460+AthullyaR@users.noreply.github.com>
2023-04-18 08:10:06 +0530

    dashboard updated

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -16,17 +16,16 @@ const Details = () => {
       tagLine={"Lets collect some data to plan your route"}/>
 
 <div className={styles.buttons}>
-     <LabelledInput 
-      
-      label={"Where are you headed to?"}
+    <h8 className={styles.inquiry}>Where are you headed to?</h8>
+     <LabelledInput  
+    //  label={"Where are you headed to?"}
       onChange={(x) => {
       console.log(x);
      }}
      className={styles.but}/>
 
+    <h8 className={styles.inquiry}>Where are you starting from?</h8>
     <LabelledInput 
-          
-          label={"Where are you starting from?"}
           onChange={(x) => {
           console.log(x);
         }}
@@ -40,13 +39,30 @@ const Details = () => {
           label={"No"}
           className={styles.smallbut}/>
     </div>
-    <LabelledInput 
-              
-              label={"What is the duration of the trip?"}
+    <h8 className={styles.inquiry}>What is the duration of the trip?</h8>
+    <LabelledInput  
+              //label={"What is the duration of the trip?"}
               onChange={(x) => {
               console.log(x);
             }}
             className={styles.but}/>
+
+<h8 className={styles.inquiry}>What is the start, end date?</h8>
+    <div className={styles.combine}>
+    <PrimaryButton  
+          label={"Start"}
+          className={styles.smallbut}/>
+    <PrimaryButton  
+          label={"End"}
+          className={styles.smallbut}/>
+    </div>
+
+    <PrimaryButton  
+          label={"Next"}
+          />
+
+
+
     </div>
 
    </div>
