79fa62cea1e9f8d2d5491eaabb5c6d27c3f904b0
ROHAN <ro.anilpnr@gmail.com>
2023-05-30 11:46:37 +0530

    axios configuration

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -4,38 +4,63 @@ import OnboardingLayout from "../components/layouts/OnboardingLayout";
 import LabelledInput from "../components/ui/LabelledInput";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Header from "../components/ui/Header";
+import axiosInstance from "../utils/axios";
+import { useState } from "react";
 
 const SignUp = () => {
+  const [data, setData] = useState({
+    name: undefined,
+    email: undefined,
+    password: undefined,
+  });
+
+  const onSubmitclick = () => {
+    axiosInstance.post("/auth/register", data);
+  };
+
   return (
     <OnboardingLayout>
       <Header title="Sign Up" tagLine={"We are your destination"} />
       <LabelledInput
         label={"Name"}
-        onChange={(x) => {
-          console.log(x);
+        onChange={(nameData) => {
+          setData((x) => {
+            return {
+              ...x,
+              name: nameData.target.value,
+            };
+          });
         }}
       />
 
       <LabelledInput
         label={"Email"}
-        onChange={(x) => {
-          console.log(x);
+        onChange={(email) => {
+          setData((x) => {
+            return {
+              ...x,
+              email: email.target.value,
+            };
+          });
         }}
       />
 
       <LabelledInput
         label={"Password"}
-        onChange={(x) => {
-          console.log(x);
+        onChange={(password) => {
+          setData((x) => {
+            return {
+              ...x,
+              password: password.target.value,
+            };
+          });
         }}
       />
 
       <PrimaryButton
         label={"Create Account"}
         //onClick={() => {}}
-        onClick={(x) => {
-          console.log('Button clicked!');
-        }}
+        onClick={onSubmitclick}
         to={"/successfull_login"}
         className={styles.cta}
       />
