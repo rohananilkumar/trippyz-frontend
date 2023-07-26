358bb9e8796163c46fa74829b6a4d780736b4d56
ROHAN <ro.anilpnr@gmail.com>
2023-07-26 14:40:09 +0530

    UI Changes

--- src/components/ui/Title.js
+++ src/components/ui/Title.js
@@ -1,18 +1,17 @@
-import React from 'react'
-import styles from './Title.module.scss'
+import React from "react";
+import styles from "./Title.module.scss";
 
-const Title = ({ titlefirst,titlesec, tagLine ,className}) => {
+const Title = ({ titlefirst, titlesec, tagLine, className }) => {
   return (
-    
     <div className={`${styles.title} ${className}`}>
-        <div className={styles.firstline}>
-             <div className={styles.headerfirst}>{titlefirst}</div>
-             <div className={styles.headersecond}>{titlesec}</div>
-        </div>
-        <div className={styles.tagLine}>{tagLine}</div>
+      <div className={styles.firstline}>
+        <div className={styles.headerfirst}>{titlefirst}</div>
+        <div className={styles.headersecond}>{titlesec}</div>
+      </div>
+      <div className={styles.tagLine}>{tagLine}</div>
+      asdfasdf
     </div>
-    
-  )
-}
+  );
+};
 
-export default Title
+export default Title;
